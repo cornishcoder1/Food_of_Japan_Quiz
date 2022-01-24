bed584faac8d715da438bc2baabfbdc5b058d5cc
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-01-24 14:09:20 +0000

    Move tab character to correct location to fix bug

--- run.py
+++ run.py
@@ -128,8 +128,8 @@ def run_quiz(quiz_data):
     for entry in quiz_data:
         print(f"{entry['question']}")
         
-        for key, value in entry['\tanswers'].items():
-            print(f"{key}: {value}")
+        for key, value in entry['answers'].items():
+            print(f"\t{key}: {value}")
 
         user_answer = input("What is your answer?\n")
         user_answer = user_answer.upper()
